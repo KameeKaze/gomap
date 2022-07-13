d5a7f767f955f89a109a2e3d502b22585a655bb2
Kamee Kaze <k4m33k4z3@protonmail.com>
2022-07-13 18:11:19 +0200

    command-line flags

--- main.go
+++ main.go
@@ -5,17 +5,37 @@ import(
 	"net"
 	"strconv"
 	"time"
+	"flag"
 )
 
+type Scanner struct {
+	Ip   string `required:"true"`
+	Port int    `required:"true"`
+	
+}
+
+// parse flags from command-line
+func (s *Scanner) Setup(){
+	flag.StringVar(&s.Ip, "i", "", "ip address")
+	flag.IntVar(&s.Port, "p", 0, "port to scan")
+}
 
 func main(){
-	open := ScanPortTCP("127.0.0.1", 3306)
+	//setup scanner
+	scanner := Scanner{}
+	scanner.Setup()
+	flag.Parse()
+
+	//scan port
+	open := ScanPortTCP(scanner.Ip, scanner.Port)
 	fmt.Println(open)
 }
 
 func ScanPortTCP(ip string, port int,) bool {
+	//parse address
 	address := ip + ":" + strconv.Itoa(port)
 
+	// check port open
 	conn, err := net.DialTimeout("tcp", address, 500*time.Millisecond)
 	if err != nil {
 		return false

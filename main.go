3576566b778568f403b58e136469dd990ad56531
Kamee Kaze <k4m33k4z3@protonmail.com>
2022-07-14 15:25:03 +0200

    goroutines

--- main.go
+++ main.go
@@ -1,52 +1,74 @@
 package main
 
-import(
+import (
+	"flag"
 	"fmt"
 	"net"
 	"strconv"
 	"time"
-	"flag"
 )
 
 type Scanner struct {
-	Ip   string 
-	Port int    
-	
+	Ip    string
+	Ports int
+}
+
+type Port struct {
+	Number int
+	Open   bool
 }
 
 // parse flags from command-line
-func (s *Scanner) Setup(){
+func (s *Scanner) Setup() {
 	flag.StringVar(&s.Ip, "i", "", "ip address")
-	flag.IntVar(&s.Port, "p", 0, "port to scan")
+	flag.IntVar(&s.Ports, "p", 0, "port to scan")
 }
 
-func main(){
+func main() {
 	//setup scanner
 	scanner := Scanner{}
 	scanner.Setup()
 	flag.Parse()
 
 	// validate ip address
-	if validateIP(scanner.Ip){
+	if validateIP(scanner.Ip) {
 		fmt.Println("Invalid ip address")
+		return
 	}
 
-	//scan port
-	open := ScanPortTCP(scanner.Ip, scanner.Port)
-	fmt.Println(open)
+	//some testing ports
+	ports := []int{22, 80, 3306}
+
+	//receive from a channel wether the port is open
+	open := ScanPortsTCP(scanner.Ip, ports)
+	for p := range open {
+		fmt.Println(p.Number, p.Open)
+	}
 }
 
-func ScanPortTCP(ip string, port int,) bool {
-	//parse address
-	address := ip + ":" + strconv.Itoa(port)
+func ScanPortsTCP(ip string, ports []int) <-chan Port {
+	open := make(chan Port)
 
-	// check port open
-	conn, err := net.DialTimeout("tcp", address, 500*time.Millisecond)
-	if err != nil {
-		return false
-	}
-	defer conn.Close()
-	return true
+	go func() {
+		//iterate over ports
+		for _, port := range ports {
+			//parse address
+			address := ip + ":" + strconv.Itoa(port)
+
+			//check if port open
+			conn, err := net.DialTimeout("tcp", address, time.Second*2)
+			switch err {
+			case nil: //open
+				defer conn.Close()
+				open <- Port{Number: port, Open: true}
+			default: // closed
+				open <- Port{Number: port, Open: false}
+			}
+		}
+		//close channel
+		close(open)
+	}()
+	return open
 }
 
 func validateIP(ip string) bool {
